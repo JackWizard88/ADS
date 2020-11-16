78a683ea089347b8a8a08c741d865f0e936980cd
Andrei Krylov <krilovavit@mail.ru>
2020-11-16 17:16:06 +0300

    added main logic

--- src/main/java/com/geekbrains/krilov/algorythms/homework6/Main.java
+++ src/main/java/com/geekbrains/krilov/algorythms/homework6/Main.java
@@ -2,13 +2,25 @@ package com.geekbrains.krilov.algorythms.homework6;
 
 public class Main {
 
-    static final int MIN = -25;
-    static final int MAX = 25;
-    static final int DEPTH = 4;
-    static final int GENERATOR_LIMIT = 15;
+    static final int NUMBER_OF_TREES = 20;      // количество деревьев в итеррации
+    static final int MAX = 25;                  // диапазон значений в дереве (тут максимум, минус будет симметричен относительно 0)
+    static final int DEPTH = 4;                 // глубина дерева
+
+    static final int MIN = -MAX;
+    static final int GENERATOR_LIMIT = (int) (Math.pow(2, DEPTH) - 1);
+
+    static int balancedTrees = 0;
 
     public static void main(String[] args) {
-        generateTree(DEPTH, GENERATOR_LIMIT, MIN, MAX).display();
+        for (int i = 0; i < NUMBER_OF_TREES; i++) {
+            generateTree(DEPTH, GENERATOR_LIMIT, MIN, MAX);
+        }
+        calculateBalancedPercent();
+    }
+
+    static void calculateBalancedPercent() {
+        double percent =  100 * (balancedTrees / (double) NUMBER_OF_TREES);
+        System.out.println("Процент сбалансированных деревьев: " + percent + "%");
     }
 
     static Tree<Integer> generateTree(int depth, int numbersLimit, int minNumber, int maxNumber) {
@@ -18,7 +30,9 @@ public class Main {
         for (int i = 0; i < 15; i++) {
             tree.add(rnd(MIN, MAX));
         }
-
+        if (tree.isBalanced()) {
+            balancedTrees++;
+        }
         return tree;
     }
 
