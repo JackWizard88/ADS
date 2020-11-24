24bf8342ccd2b3922cbef5d109e12030775b58d4
Andrei Krylov <krilovavit@mail.ru>
2020-11-24 22:03:17 +0300

    hw8 done

---
+++ src/main/java/com/geekbrains/krilov/algorythms/homework8/HashTable.java
@@ -0,0 +1,26 @@
+package com.geekbrains.krilov.algorythms.homework8;
+
+public interface HashTable<K, V> {
+
+    boolean put(K key, V value);
+
+    V get(K key);
+
+    V remove(K key);
+
+    int size();
+
+    boolean isEmpty();
+
+    void display();
+
+    interface Entry<K, V> {
+
+        K getKey();
+
+        V getValue();
+
+        void setValue(V value);
+    }
+
+}
---
+++ src/main/java/com/geekbrains/krilov/algorythms/homework8/Main.java
@@ -0,0 +1,36 @@
+package com.geekbrains.krilov.algorythms.homework8;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HashTable<Product, Integer> hashTable = new LinkedHashTableImpl<>(10);
+
+        hashTable.put(new Product(1, "Orange"), 150);
+        hashTable.put(new Product(77, "Banana"), 100);
+        hashTable.put(new Product(77, "Carrot"), 228);
+        hashTable.put(new Product(60, "Lemon"), 250);
+        hashTable.put(new Product(52, "Milk"), 120);
+        hashTable.put(new Product(21, "Potato"), 67);
+
+        System.out.println("Size is " + hashTable.size());
+        hashTable.display();
+
+        System.out.println("Cost potato is " + hashTable.get(new Product(21, "Potato")));
+        System.out.println("Cost banana is " + hashTable.get(new Product(77, "Banana")));
+
+        hashTable.remove(new Product(21, "Potato"));
+        hashTable.remove(new Product(77, "Banana"));
+
+        System.out.println("Cost potato is " + hashTable.get(new Product(21, "Potato")));
+        System.out.println("Cost banana is " + hashTable.get(new Product(77, "Banana")));
+
+        hashTable.put(new Product(52, "Milk"), 120);
+        hashTable.put(new Product(21, "Potato"), 67);
+
+        System.out.println("Cost potato is " + hashTable.get(new Product(21, "Potato")));
+        System.out.println("Cost milk is " + hashTable.get(new Product(52, "Milk")));
+
+        hashTable.display();
+    }
+}
